e567cf3114c064a2b1c6b3e4c4b7bc1456560a60
olmaksymova <92872822+olmaksymova@users.noreply.github.com>
2021-10-20 22:54:03 +0300

    Add files via upload
    Лабораторна робота завершена

---
+++ Лабораторна робота №1/Завдання 1/Завдання_1.cs
@@ -0,0 +1,54 @@
+﻿
+using System;
+using static System.Console;
+using static System.Convert;
+using static System.Math;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            double x, y, z, a, b;
+            
+            
+            
+            try
+            {
+                Write("Введiть x: ");
+                x = ToDouble(ReadLine());
+                Write("Введiть y: ");
+                y = ToDouble(ReadLine());
+                Write("Введiть z: ");
+                z = ToDouble(ReadLine());
+                if (z == 0 || x == 0 || x < y)
+                    WriteLine("Данi числа не задовольняють ОДЗ");
+                else
+                {
+                    double ch, zn;
+                    ch = Pow(Abs(y) + Pow(z, 3), 1.0 / 3);
+                    zn = Pow(x, 3) + x;
+                    a = x + ch / zn;
+                    if (a != 0)
+                    {
+                        b = Sqrt(x - y) / z + 1 / a / a;
+                        WriteLine("a = {0}\tb = {1}", a,b);
+                    }
+                    else
+                        WriteLine("Данi числа не задовольняють ОДЗ");
+                }
+            }
+            catch
+            {
+                WriteLine("Помилка вводу даних");
+            }
+            
+               
+                
+
+        }
+    }
+}
---
+++ Лабораторна робота №1/Завдання 2/Задача_2.cs
@@ -0,0 +1,63 @@
+﻿
+using System;
+using static System.Console;
+using static System.Convert;
+using static System.Math;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool Prime_or_not(long N)
+            {
+                long j;bool prime=true;
+                for (j = 2; j <= Sqrt(N); j++)
+                    if (N % j == 0)
+                    {
+                        prime = !prime;
+                        break;
+                    }
+                return prime;
+            }
+            long n, Mersen_number;
+            
+            long i, j;
+            Write("Введiть n: ");
+            try
+            {
+                n = ToInt32(ReadLine());
+            }
+            catch
+            {
+                n = 0;
+            }
+            bool prime;
+            if (n > 0)
+            {
+                if (n > 3)
+                {
+                    Write("Числа Мерсена: ");
+                    for (i = 2; i < n; i++)
+                    {
+                        prime = Prime_or_not(i);
+                        Mersen_number = (long)Pow(2, i) - 1;
+                        if (Mersen_number < n)
+                        {
+                            if (prime && Prime_or_not(Mersen_number)==true)
+                                Write($"{Mersen_number} ");
+                        }
+                        else
+                            break;
+                    }
+                }
+                else
+                    WriteLine($"Чисел Мерсена, менших за {n}, не iснує ");
+            }
+            else
+                WriteLine("Помилка вводу даних");
+
+        }
+    }
+}
