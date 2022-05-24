556adc0a5fc0c71c669ef1196e22dd9b67676e0f
olmaksymova <92872822+olmaksymova@users.noreply.github.com>
2022-05-24 23:10:42 +0300

    Add files via upload
    Лабораторна робота завершена. Помилково замість 9 варіанту було виконано варіант за списком - 12. Дякую за розуміння.

---
+++ Лабораторна робота №6/Deque.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using static System.Console;
+
+namespace lab6
+{
+    public class Node
+    {
+        public char data;
+        public Node next;
+        public Node prev;
+        public Node(char data)
+        {
+            this.data = data;
+            this.prev = null;
+            this.next = null;
+        }
+    }
+    public class Deque
+    {
+        private Node Head;
+        private Node Tail;
+        private int Size;
+        public Deque()
+        {
+            Head = null;
+            Tail = null;
+            Size = 0;
+        }
+        public bool isEmpty()
+        {
+            if (this.Head == null)
+                return true;
+            else
+                return false;
+        }
+        public void insertHead(char buffer)
+        {
+            Node node = new Node(buffer);
+            node.next = this.Head;
+            if (this.Head == null)
+            {
+                this.Head = node;
+                this.Tail = node;
+            }
+            else
+            {
+                this.Head.prev = node;
+                this.Head = node;
+            }
+            this.Size++;
+            this.printDeque();
+        }
+        public Deque Copy()
+        {
+            Deque clone = new Deque();
+                Node node = this.Head;
+                while (node != null)
+                {
+                    clone.insertTail(node.data);
+                    node = node.next;
+                }
+                return clone;
+        }
+        public void insertTail(char buffer)
+        {
+            Node node = new Node(buffer);
+            if (this.Head == null)
+            {
+                this.Head = node;
+                this.Tail = node;
+            }
+            else
+            {
+                node.prev = this.Tail;
+                this.Tail.next = node;
+                this.Tail = node;
+            }
+            this.Size++;
+            this.printDeque();
+        }
+        public void removeTail()
+        {
+            if (this.isEmpty() == true)
+            {
+                return;
+            }
+            this.Tail = this.Tail.prev;
+
+            if (this.Tail == null)
+            {
+                this.Head = null;
+            }
+            else
+            {
+                this.Tail.next = null;
+            }
+            this.Size--;
+
+            this.printDeque();
+        }
+        public void removeHead()
+        {
+            if (this.isEmpty() == true)
+            {
+                return;
+            }
+            this.Head = this.Head.next;
+            if (this.Head == null)
+            {
+                this.Tail = null;
+            }
+            else
+            {
+                this.Head.prev = null;
+            }
+            this.Size--;
+            this.printDeque();
+        }
+
+        public Node head() => this.Head;
+
+        public Node tail() => this.Tail;
+        public int size() => this.Size;
+        public void printDeque()
+        {
+            Node node = this.Head;
+            if (node != null)
+            {
+                WriteLine("\nДана черга");
+                while (node != null)
+                {
+                    Write(node.data);
+                    node = node.next;
+                }
+            }
+            else
+                WriteLine("\nДана черга порожня!");
+            ResetColor();
+            WriteLine();
+        }
+        public bool isPalindrome()
+        {
+            WriteLine("\n************************Створення копiї черги************************");
+            Deque current = this.Copy();
+
+            if (!current.isEmpty())
+            {
+                WriteLine("\n************************Видалення елементiв з копiї************************");
+                do
+                {
+                    if (current.Head.data == current.Tail.data)
+                    {
+                        current.removeTail();
+                        current.removeHead();
+                    }
+                    else
+                        return false;
+                }
+                while (!current.isEmpty());
+                return true;
+            }
+            else return false;
+        }
+        public void printHalfDeque()
+        {
+            string half = "";
+            WriteLine("\n************************Створення копiї черги************************");
+            Deque current = this.Copy();
+
+            WriteLine("\n************************Видалення елементiв з копiї************************");
+            while (!current.isEmpty())
+            {
+                half += current.Tail.data;
+
+                current.removeHead();
+                current.removeTail();
+            }
+            Program.ColorWriteLine($"Половина даної черги\n{half}");
+        }
+        public void printTransposition()
+        {
+            WriteLine("\n************************Перестановка правої половини деку на його початок************************");
+            for (int i = 0; i < this.Size/2f; i++)
+            {
+                this.insertHead(this.Tail.data);
+                this.removeTail();
+            }
+
+            ForegroundColor = ConsoleColor.DarkRed;
+            BackgroundColor = ConsoleColor.White;
+            this.printDeque();
+            ResetColor();
+        }
+    }
+}
---
+++ Лабораторна робота №6/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using static System.Console;
+namespace lab6
+{
+
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            Deque q = new Deque();
+            string command = "1", example = "abccba";
+            string Input;
+            bool result = false;
+
+            while (command != "2")
+            {
+                ColorWriteLine("Оберiть команду (\"0\" - контрольний приклад,\"1\" - ваша строка,\"2\" - вихiд):");
+                command = ReadLine();
+
+                if (command == "1")
+                {
+                    do
+                    {
+                        q = new Deque();
+                        ColorWriteLine("Введiть строкy: ");
+                        Input = ReadLine();
+                        
+                        while (Input.Length % 2 != 0 || Input.Length == 0)
+                        {
+                            ColorWriteLine("Введiть число символiв кратне двом: ");
+                            Input = ReadLine();
+                        }
+
+                        StringToDeque(q, Input);
+                        result = q.isPalindrome();
+
+                        if (!result)
+                        {
+                            ColorWriteLine("Дана строка не палiндром. Введiть iншу: ");
+                        }
+                    }
+                    while (!result);
+                    printResult(q);
+                }
+
+                else if (command == "0")
+                {
+                    ColorWriteLine("Строка-приклад: " + example);
+
+                    StringToDeque(q, example);
+
+                    if (q.isPalindrome())
+                        printResult(q);
+                    else
+                    {
+                        ColorWriteLine("Дана строка не є палiндромом");
+                    }
+                }
+
+                else if (command == "2")
+                    break;
+                else
+                {
+                    ColorWriteLine("Невiрно введенi данi!");
+                }
+            }
+        }
+        static public void printResult(Deque q)
+        {
+            ColorWriteLine("Дана строка є палiндромом!");
+            q.printHalfDeque();
+            q.printTransposition();
+            WriteLine();
+        }
+        static public void StringToDeque(Deque q, string input)
+        {
+            for (int i = 0; i < input.Length; i++)
+                q.insertTail(input[i]);
+
+            ForegroundColor = ConsoleColor.DarkRed;
+            BackgroundColor = ConsoleColor.White;
+            q.printDeque();
+            ResetColor();
+        }
+        static public void ColorWriteLine(string output)
+        {
+            ForegroundColor = ConsoleColor.DarkRed;
+            BackgroundColor = ConsoleColor.White;
+            Write(output);
+            ResetColor();
+            WriteLine();
+        }
+    }
+
+}
